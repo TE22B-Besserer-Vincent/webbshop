d911e66e6c42918222d0418a0303ce799e18c5d1
TE22B-Besserer-Vincent <vincent.besserer@elev.ga.ntig.se>
2023-11-24 14:53:25 +0100

    ändrat på knapparnas positioner

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Hello!")
+
+function myFunction() {
+  console.log("Hello again!")
+  console.log(document.getElementsByClassName("dropdown-content"))
+}
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.cart-btn') || !event.target.matches('.cart-dropdown')) {
+      var dropdown = document.getElementsByClassName("dropdown-content")[0];
+      if (dropdown.style.display == "flex") {
+        dropdown.style.display = "none"
+        document.getElementsByClassName("screen-tint")[0].style.display = "none"
+      } else { dropdown.style.display = "flex"; document.getElementsByClassName("screen-tint")[0].style.display = "flex";  }
+    }  
+} 
